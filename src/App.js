f64053b44744c17fe3cbd0b1961e7bcb8fbf1ce2
InnaGusarova <g21022015v@gmail.com>
2021-04-15 12:34:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NewTodo from './NewToDo'
+import styles from './index.module.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+    value: '',
+    newList: [],
+    inputError: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+  
+  handleClick = () => {
+    const { value, newList } = this.state;
+    if (!value) {
+      return;
+    }
+    if (newList.some(list => list.value === value)) {
+      return this.setState({ inputError: true })
+    }
+    const newListClone = [...newList];
+    newListClone.push({ value });
+    this.setState({ value: '', newList: newListClone, inputError: false });
+  }
+
+  handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+    this.handleClick();
+    }
+  }
+
+  removeNewTodo = (i) => {
+    const newList = [...this.state.newList];
+    newList.splice(i, 1);
+    this.setState({ newList: newList });
+  } 
+
+  render() {
+    const { newList, value } = this.state;
+    return(
+      <React.Fragment>
+        <div className={styles.wrapperTitle}>
+          <h1 className={styles.title}>Не забыть ...</h1>
+          <input 
+            type="text"
+            value={value}
+            placeholder="Имя списка"
+            className={styles.inputTitle}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          <p
+            className={styles.inputError}
+            style={{color: this.state.inputError ? 'rgb(206, 23, 23)' : 'rgb(182, 233, 233)'}}
+          >
+            Список с таким именем мы уже бережно храним для Вас на стикерах ниже. Выберите новое имя для следующего списка.</p>
+          <button 
+            className={styles.submitTitle}
+            onClick={this.handleClick}
+          > 
+            Создать 
+          </button>
+        </div>
+        <div className={styles.wrapperTodo}>
+          {newList.map((item, i) => {
+            return(
+              <div 
+              className={styles.todo}
+              key={item.value}>
+                <NewTodo 
+                  id={i} 
+                  remove={this.removeNewTodo}
+                  title={item.value} 
+                  newList={item.newList}
+                />
+              </div>
+            )
+           })}
+        </div>
+      </React.Fragment>
+    )
+  }
+   
 }
-
-export default App;
+export default App
---
+++ src/NewToDo.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+class NewTodo extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state={ 
+        value: '',
+        todoList: [],
+        };
+    }
+    
+    handleChange = (event) => {
+        this.setState({ value: event.target.value});
+    }
+    handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+        this.handleClick();
+        }
+    }
+ 
+    handleClick = () => {
+        if (!this.state.value) {
+            return;
+        }
+        const todoList = [...this.state.todoList];
+        todoList.push({ value: this.state.value, checked: false });
+        this.setState({ todoList: todoList, value: '' });
+    }
+
+    handleCheked = (i) => {
+        const todoList = [...this.state.todoList];
+        todoList[i].checked = true;
+        this.setState({ todoList: todoList });
+    }
+
+    handleDelete = (i) => {
+        const todoList = [...this.state.todoList];
+        todoList.splice(i, 1);
+        this.setState({ todoList: todoList });
+    }
+    
+    handleKill  = () => {
+        const { remove, id } = this.props;
+        remove(id);
+    }
+    render() {
+        const { value, todoList } = this.state;
+        const { title } = this.props;
+        return (
+            <React.Fragment>
+                <div className={styles.wrapperTodoTitle}>
+                    <h2 className={styles.todoTitle}>
+                        {title}
+                    </h2>
+                    <button 
+                        className={styles.deleteTodo}
+                        onClick={this.handleKill}
+                    >
+                        &#10006;
+                    </button>
+                </div>
+                <div className={styles.wrapperInpTodo}>
+                    <input 
+                        type="text"
+                        value={value} 
+                        placeholder="напомнить мне ..."
+                        className={styles.inputTodo}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKeyDown}
+                    />
+                    <button 
+                        className={styles.addTodo}
+                        onClick={this.handleClick}
+                    >
+                        &#9998;
+                    </button>
+                </div>
+                {todoList.map((item, i) => {
+                    return(
+                    <div 
+                        key={i}
+                        className={styles.textWrapper}
+                    >
+                        <p 
+                            className={styles.textTodo} 
+                            style={{textDecoration: item.checked ? 'line-through' : 'none'}}
+                        >
+                            {item.value}
+                        </p>
+                        <button 
+                            className={styles.chekedTodo}
+                            onClick={() => this.handleCheked(i)}
+                        >
+                            &#10004;
+                        </button>
+                        <button 
+                            className={styles.deleteTodo}
+                            onClick={() => this.handleDelete(i)}
+                        >
+                            &#10006;
+                        </button>
+                    </div>
+                    )
+                })}
+            </React.Fragment>
+        )
+    }
+}
+
+export default NewTodo
